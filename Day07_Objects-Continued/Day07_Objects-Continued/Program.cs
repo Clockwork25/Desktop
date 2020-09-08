fa5467ed00b3109a7c8ba1bbccf58a4e13483630
Clockwork25 <67840273+Clockwork25@users.noreply.github.com>
2020-09-08 09:36:32 -0700

    Day 7 Task 1
    Day 7 Task 1 pabeigts 08.09.2020

---
+++ Day07_Objects-Continued/Day07_Objects-Continued/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Day07_Objects_Continued
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students klasesbiedrs = new Students();
+
+            Console.WriteLine("Vards");
+            klasesbiedrs.vards = Console.ReadLine();
+
+            Console.WriteLine("Uzvards");
+            klasesbiedrs.uzvards = Console.ReadLine();
+
+            Console.WriteLine("Kurss");
+            try 
+            {
+                klasesbiedrs.kurss = Convert.ToInt32(Console.ReadLine());
+
+            }
+            catch
+            {
+                Console.WriteLine("Kļuda, vai ievadiji skaitļus?");
+            }
+
+            klasesbiedrs.PrintInfo();
+        }
+    }
+}
---
+++ Day07_Objects-Continued/Day07_Objects-Continued/Students.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day07_Objects_Continued
+{
+    class Students
+    {
+
+        public String vards { get; set; }
+
+        public String uzvards;
+
+        private int tempKurss;
+
+        public int kurss
+        {
+
+            get
+            {
+                return tempKurss;
+            }
+            set
+            {
+                if (value >= 1 && value <= 3)
+                {
+                    tempKurss = value;
+                }
+                else if (value > 3)
+                {
+                    tempKurss = 3;
+                }
+                else
+                {
+                    tempKurss = 1;
+                }
+            }
+
+        }
+        public void Test()
+        {
+            Console.WriteLine("TEst OK");
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(vards + " " + uzvards + " " + kurss);
+        }
+    }
+}
