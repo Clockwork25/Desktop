1e9e116fb39d98132fba206f38c4ffbb5deb22b6
Clockwork25 <67840273+Clockwork25@users.noreply.github.com>
2020-09-11 09:27:19 -0700

    Day9

---
+++ Day9_IO-files/Day9_IO-files/FileOperations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Day9_IO_files
+{
+    class FileOperations {
+        public static string Path { get ; set; }
+
+        public static string File()
+    {
+        Console.WriteLine("Ievadi faila nosaukumu");
+             Path = Console.ReadLine();
+            return Path;
+    }
+        public static void Write()
+        {
+            try
+            {
+                //                                    fails tiek papildināts, ja true
+                StreamWriter sw = new StreamWriter(File());
+                //                                           ^^^^^^^^^
+                //                                    fails tiek pārrakstīts, ja false, vai ir atstāts  tukšs
+                sw.WriteLine("Hello ");
+                sw.WriteLine("world!");
+                sw.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Nevareja izpildīt rakstīšanu");
+            }
+        }
+        public static void Read()
+        {
+            try
+            {
+                Console.WriteLine(Path);
+              /*  StreamReader sr = new StreamReader("Test.txt");
+
+                String line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();*/
+
+            }
+            catch
+            {
+                Console.WriteLine("Nope, \n nav faila");
+
+            }
+        }
+    }
+}
---
+++ Day9_IO-files/Day9_IO-files/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day9_IO_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileOperations.Write();
+
+            FileOperations.Read();
+        }
+    }
+}
